98fda9c96d646c4a47392af272a14382879fa7e5
hnyln <hjcarocoy@gmail.com>
2025-12-23 22:14:30 +0800

    bottom-sheet

--- src/app/screens/(tabs)/bookmark.tsx
+++ src/app/screens/(tabs)/bookmark.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native";
+import React, { useRef } from "react";
+import { Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import HomeScreen from ".";
 
 export default function Bookmark() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
   return (
-    <View>
-      <Text className="text-lg">This uses Poppins-Regular by default</Text>
-      <Text className="font-sans-bold text-lg">Bold text</Text>
-      <Text className="font-sans-medium text-lg">Medium text</Text>
-    </View>
+    <GestureHandlerRootView className="flex-1 bg-white">
+      {/* main content */}
+      <HomeScreen />
+
+      {/* drawer */}
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={["10%", "50%", "90%"]}
+        index={3}
+      >
+        <BottomSheetView className="flex-1">
+          <View className="p-4">
+            <Text className="font-sans-bold text-2xl">Bookmarks</Text>
+          </View>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 }
